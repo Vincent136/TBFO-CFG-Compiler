acfd4c0ce3bdbc2bf048e09f2e1b3ea17baa307c
geraldakbar <mgeraldakbar@gmail.com>
2021-11-22 22:09:43 +0700

    text file reader

---
+++ readFile.py
@@ -0,0 +1,27 @@
+
+import re
+
+def readInput(fileName):
+    # Read from file
+    with open(fileName, 'r') as f:
+        contents = f.read()
+        contents = contents.split()
+
+        result = contents
+        # For each operator..
+        temporaryResult = []
+        # For each statement..
+        for statement in result:
+            x = re.split('(\W)', statement)
+            # add the split result to temporaryResult
+            for splitStatement in x:
+                temporaryResult.append(splitStatement)
+
+        result = temporaryResult
+
+        # remove empty string
+        result = [string for string in result if string != '']
+        print(result)
+        return result
+
+readInput('test.txt')
