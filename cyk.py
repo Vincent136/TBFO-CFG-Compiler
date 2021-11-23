4bcc36b964a3374b71e4cb1dce8038acd393fea9
Vincent136 <13520136@std.stei.itb.ac.id>
2021-11-23 16:15:58 +0700

    fix cfg + cyk

--- cyk.py
+++ cyk.py
@@ -1,81 +1,82 @@
 import re
 import sys
-import readFile as rf
+import readFile as rf 
 
-# Regex List
-regexList = [r'[A-z0-9]*', r'[0-9]*', r'[A-Za-z_][A-Za-z_0-9]*']
 
-# Maps regex into valid key
-regexMap = {
-    r'[A-z0-9]*': ["string"],
-    r'[0-9]*': ["number"],
-    r'[A-Za-z_][A-Za-z_0-9]*': ["variable"],
+regex = [r'[A-z0-9]*',
+             r'[0-9]*', 
+             r'[A-Za-z_][A-Za-z_0-9]*']
+
+
+
+
+regexDictionary = {
+    r'[A-z0-9]*': "string",
+    r'[0-9]*': "number",
+    r'[A-Za-z_][A-Za-z_0-9]*': "variable",
 }
 
-global ChG
-ChG = {}
 
 
-def LoadCNF(modelPath):
+def bacaCNF(modelPath):
+    ChG = []
     file = open(modelPath).read()
-    rawRules = file.split('\n')
-    for i in range(len(rawRules)-1):
-        A = rawRules[i].split(' -> ')[0]
-        B = rawRules[i].split(' -> ')[1]
-        B = B.replace(" ", "")
-        C = B.split('|')
-        for j in range(len(C)):
-            value = ChG.get(C[j])
-            if (value == None):
-                ChG.update({C[j]: [A]})
-            else:
-                ChG[C[j]].append(A)
+    Rules = file.split('\n')
+    for i in range(len(Rules)-1):
+        HEAD = Rules[i].split(' -> ')[0]
+        TAIL = Rules[i].split(' -> ')[1]
+        TAIL = TAIL.replace(" ", "")
+        CTAIL = TAIL.split('|')
+        tuple = HEAD, CTAIL
+        ChG.append(tuple)
+    return ChG
 
 
-def cyk(Token):
+def cyk(Token, ChG):
 
-    TableOut = [[[] for j in range(i)]
-                for i in range(len(Token), 0, -1)]
+    TableOut = [[[] for j in range(len(Token))]for i in range(len(Token))]
+    
     for i in range(len(Token)):
-        try:
-            TableOut[0][i].extend(ChG[Token[i]])
-        except KeyError:
-            for P in regexList:
-                if(re.match(P, Token[i])):
-                    for regexType in regexMap[P]:
-                        try:
-                            TableOut[0][i].extend(ChG[regexType])
-                        except KeyError:
-                            continue
-
+        for item in ChG:
+            if(Token[i] in item[1]):
+                TableOut[0][i].extend([item[0]])
+        if len(TableOut[0][i]) == 0:
+            for TestRegex in regex:
+                if(re.match(TestRegex, Token[i])):
+                    for item in ChG:
+                        if (regexDictionary[TestRegex] in item[1]) :
+                            TableOut[0][i].extend([item[0]])
+        
+    
     for i in range(1, len(Token)):
         for j in range(len(Token)-i):
             for k in range(i):
                 for P1 in TableOut[i-k-1][j]:
                     for P2 in TableOut[k][j+i-k]:
-                        try:
-                            TableOut[i][j] = ChG[P1 + P2]
-                        except KeyError:
-                            continue
-    return TableOut
+                        for item in ChG:
+                            if(P1 + P2 in item[1]):
+                                TableOut[i][j].extend([item[0]])
 
-
-def checkValidity(table, wanted):
-    return wanted in table[-1][-1]
+    return(TableOut)
+                        
 
 
 # tess
 if len(sys.argv) > 1:
-    modelPath = str(sys.argv[1])
+    input = str(sys.argv[1])
 else:
-    modelPath = 'cnf.txt'
+    input = 'input.txt'
 
-LoadCNF(modelPath)
+ChG = bacaCNF('CNF.txt')
 
-inp = rf.readInput("test.txt")
-inp2 = cyk(inp)
 
-if (checkValidity(inp2, "S")):
-    print("Verdict accepted! Compile success!")
+inp = rf.readInput(input)
+
+print("Compiling....")
+
+Parse = cyk(inp, ChG)
+
+if ("START" in Parse[-1][0] or "COMMENT" in Parse[-1][0]):
+    print("Compile Succes!")
 else:
-    print("Compile error, wrong syntax!")
+    print("Syntax error : Invalid syntax")
