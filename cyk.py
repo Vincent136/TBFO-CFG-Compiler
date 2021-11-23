2bd0c111e08fa62737d690e2771471dc44fb761b
Vincent136 <13520136@std.stei.itb.ac.id>
2021-11-24 02:31:27 +0700

    anjay

--- cyk.py
+++ cyk.py
@@ -1,6 +1,7 @@
 import sys
 
-number = ['1','2','3','4','5','6','7','8','9','10']
+number = ['1','2','3','4','5','6','7','8','9','0']
+variable = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', '_']
 
 def checkNumber(string):
     for item in string:
@@ -9,10 +10,10 @@ def checkNumber(string):
     return True
 
 def checkVariable(string):
-    if string[0] in number:
-        return False
-    else:
+    if string[0] in variable:
         return True
+    else:
+        return False
 
 
 def bacaCNF(modelPath):
